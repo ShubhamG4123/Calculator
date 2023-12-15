eaa8443a23b93d8a0b690ba73a9f0264d9c55e0e
ShubhamG4123 <100066162+ShubhamG4123@users.noreply.github.com>
2023-12-15 14:44:15 +0530

    Add files via upload

---
+++ calci.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char op;
+    double num1, num2;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    switch(op) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+        case '/':
+            if(num2 != 0)
+                cout << num1 << " / " << num2 << " = " << num1 / num2;
+            else
+                cout << "Cannot divide by zero!";
+            break;
+        default:
+            cout << "Invalid operator!";
+    }
+
+    return 0;
+}
